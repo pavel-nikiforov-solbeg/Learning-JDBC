2f82e7f98b64cb65cd682f86614000c6d2920a29
Pavel Nikiforov <pavel.nikiforov@solbeg.com>
2024-07-22 15:52:01 +0300

    The simplest possible example

---
+++ src/main/java/by/gexateq/nikiforov/App.java
@@ -0,0 +1,38 @@
+package by.gexateq.nikiforov;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class App 
+{
+    private static final String DB_URL = "jdbc:h2:mem:test";
+    private static final String CREATE_TABLE_USERS = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+    private static final String INSERT_DATA = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+    private static  final String SHOW_ALL_USERS = "SELECT * FROM users";
+
+    public static void main( String[] args ) throws SQLException
+    {
+        Connection connection = DriverManager.getConnection(DB_URL);
+        var statement = connection.createStatement();
+        statement.execute(CREATE_TABLE_USERS);
+        statement.close();
+
+        var statement2 = connection.createStatement();
+        statement2.executeUpdate(INSERT_DATA);
+        statement2.close();
+
+        var statement3 = connection.createStatement();
+        var resultSet = statement3.executeQuery(SHOW_ALL_USERS);
+        while(resultSet.next()){
+            log.info("Username: {}", resultSet.getString("username"));
+            log.info("Phone: {}", resultSet.getString("phone"));
+        }
+        statement3.close();
+
+        connection.close();
+    }
+}
