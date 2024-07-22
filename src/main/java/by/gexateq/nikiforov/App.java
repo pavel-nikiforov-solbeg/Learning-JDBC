c2fb4bcd2d3f33ed1fcb25bf73b733f06a3fc87e
Pavel Nikiforov <pavel.nikiforov@solbeg.com>
2024-07-22 16:31:11 +0300

    Added try-with-resources

--- src/main/java/by/gexateq/nikiforov/App.java
+++ src/main/java/by/gexateq/nikiforov/App.java
@@ -12,27 +12,26 @@ public class App
     private static final String DB_URL = "jdbc:h2:mem:test";
     private static final String CREATE_TABLE_USERS = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
     private static final String INSERT_DATA = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-    private static  final String SHOW_ALL_USERS = "SELECT * FROM users";
+    private static  final String SHOW_ALL_USERS = "SELECT username, phone FROM users";
 
     public static void main( String[] args ) throws SQLException
     {
-        Connection connection = DriverManager.getConnection(DB_URL);
-        var statement = connection.createStatement();
-        statement.execute(CREATE_TABLE_USERS);
-        statement.close();
+        try (Connection connection = DriverManager.getConnection(DB_URL)) {
+            try (var statement = connection.createStatement()) {
+                statement.execute(CREATE_TABLE_USERS);
+            }
 
-        var statement2 = connection.createStatement();
-        statement2.executeUpdate(INSERT_DATA);
-        statement2.close();
+            try (var statement2 = connection.createStatement()) {
+                statement2.executeUpdate(INSERT_DATA);
+            }
 
-        var statement3 = connection.createStatement();
-        var resultSet = statement3.executeQuery(SHOW_ALL_USERS);
-        while(resultSet.next()){
-            log.info("Username: {}", resultSet.getString("username"));
-            log.info("Phone: {}", resultSet.getString("phone"));
+            try (var statement3 = connection.createStatement()) {
+                var resultSet = statement3.executeQuery(SHOW_ALL_USERS);
+                while (resultSet.next()) {
+                    log.info("Username: {}", resultSet.getString("username"));
+                    log.info("Phone: {}", resultSet.getString("phone"));
+                }
+            }
         }
-        statement3.close();
-
-        connection.close();
     }
 }
